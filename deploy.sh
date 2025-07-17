d8ebd83ba9029bf12280cbc54de0ddeaba938aba
Chike Abuah <abuahchu@gmail.com>
2025-07-17 05:48:32 -0400

    more or less print ready

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,9 @@
-# build cn book
-echo "# build cn book"
-jupyter-book build zh_cn/notebooks
-jupyter-book build --builder pdflatex zh_cn/notebooks
-
-# build en book
+# build
 echo "# build en book"
 jupyter-book build notebooks
 jupyter-book build --builder pdflatex notebooks
 
-# cp cn assets
-echo "# cp cn assets"
-mkdir -p _build/html/cn
-cp -R zh_cn/_build/html/*  _build/html/cn/
-cp zh_cn/static/index.html _build/html/cn/
-cp zh_cn/static/book-logo.png _build/html/cn/
-cp zh_cn/_build/latex/cn_book.pdf _build/html/cn 2>/dev/null || :
-
-# cp en assets
+# copy static files
 echo "# cp en assets"
 cp static/index.html _build/html/
 cp static/book-logo.png _build/html/
